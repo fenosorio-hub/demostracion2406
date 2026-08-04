86ffaf61a4749acd32c86ee015c4409872c2f929
Stark <fenosorio@gmail.com>
2026-08-03 23:21:31 -0300

    Delete 20240520000000_assign_admin_role.sql

--- 20240520000000_assign_admin_role.sql
+++
@@ -1,6 +0,0 @@
--- Le asigna el rol de admin al usuario en la tabla de perfiles (si usas esa tabla)
-UPDATE public.profiles
-SET role = 'admin'
-WHERE id IN (
-  SELECT id FROM auth.users WHERE email = 'FENOSORIO@gmail.com'
-);
